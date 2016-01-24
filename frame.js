93f7c8dc92fbac312bd13516d38c73deda2ca119
John Villa <jhn735@gmail.com>
2016-01-23 21:23:12 -0800

    added draw_image_data and convert_dimensions functions

--- frame.js
+++ frame.js
@@ -18,7 +18,7 @@ function canvas_frame(parent_frame, width, height, percent){
 	this.coords = {x:0, y:0};
 
 	this.background = "none";
-	var foreground = new ImageData(this.width, this.height);
+
 	//set the canvas of this frame and if applicable, 
 		//add this frame to it's parent's children list
 	if(this.parent_frame instanceof HTMLCanvasElement){ 
@@ -64,7 +64,6 @@ function canvas_frame(parent_frame, width, height, percent){
 		return that.frame_children.push(n_wind);
 		//if the child is a canvas_frame just add it to the list of children	
 		}else if(typeof child === typeof that){ return that.frame_children.push(child);}
-
 	}
 
 	// This function clears and redraws whatever falls within this frame. 
@@ -74,8 +73,8 @@ function canvas_frame(parent_frame, width, height, percent){
 		that.clear();
 		that.draw(that.background);
 		for(i = 0; i < that.frame_children.length; i++){
-			that.draw(frame_children[i]);
 			frame_children[i].refresh();				
+			that.draw(frame_children[i]);
 		}
 	}	
 	
@@ -85,8 +84,11 @@ function canvas_frame(parent_frame, width, height, percent){
 		that.context.clearRect(that.coords.x, that.coords.y, that.width, that.height);
 	}
 
+	/** Sets the current image displayed as the background **/
 	this.set_background = function(obj){
-		if(typeof obj !== "undefined" ) {that.draw(obj);}
+		if(typeof obj !== "undefined" ) {
+			that.clear();
+			that.draw(obj);}
 		var coords = that.convert_coords(that.coords.x, that.coords.y);
 		background = that.context.getImageData(coords.x, coords.y, that.width, that.height);
 	};
@@ -94,15 +96,12 @@ function canvas_frame(parent_frame, width, height, percent){
 /* Draws either the given image at the given coordsinates or fills with the given
 	 color. If either x_coords or y_coords is undefined they are assumed to be 0.
 */
-	this.draw = function(obj, x_coords, y_coords){
+	this.draw = function(obj, x_coord, y_coord){
 		//if the object isn't anything don't do anything
 		if(typeof obj === "undefined") return;
 		//check the types of the coordinates, if not numbers, set to 0
-		if(typeof x_coords !== "number"){ x_coords = 0;}
-		if(typeof y_coords !== "number"){ y_coords = 0;}
-
-		//convert the coordsinates into global coordsinates
-		var coords = that.convert_coords(x_coords, y_coords);
+		if(typeof x_coord !== "number"){ x_coord = 0;}
+		if(typeof y_coord !== "number"){ y_coord = 0;}
 
 		//if the drawing object is an image
 			//draw the part that falls within the frame
@@ -111,26 +110,10 @@ function canvas_frame(parent_frame, width, height, percent){
 		}else if(obj instanceof ImageData){
 			
 		}else if(typeof obj === "string"){
-			if(/t(ext)?:.+/.test(obj)){ that.draw_obj(obj.replace(/t(ext)?:/, ""), coords.x, coords.y); }
+			if(/t(ext)?:.+/.test(obj)){ 
+				that.draw_text(obj.replace(/t(ext)?:/, ""), coords.x, coords.y); }
 			else{ that.fill_color(obj); }
 		}
-	}
-	/** Returns a collection dimensions and coordinates appropriate for drawing images **/
-	this.convert_dimensions = function(x, y, width, height){
-			var dim = {};
-			dim.x = x; dim.y = y;
-			//set up sx and sy
-			dim.sx = 0; dim.sy = 0;
-			if(x < 0){ dim.sx = 0-x; dim.x = 0;}
-			if(y < 0){ dim.sy = 0-y; dim.y = 0;}
-
-			//set up swidth and sheight
-			dim.swidth = 0; dim.sheight = 0;
-			if( (dim.x + width) > that.width){ 
-				dim.swidth = that.width - dim.x;}
-			if( (dim.y + height) > that.height){ 
-				dim.sheight = that.height - dim.y;}
-	return dim;
 	}
 
 /* Draws the image that either fills the frame or clips it*/
@@ -140,11 +123,11 @@ function canvas_frame(parent_frame, width, height, percent){
 		if(typeof y_coords !== "number"){ y_coords = 0;}
 			
 			//change the dimensions if needed
-			var dim = convert_dimensions(x, y, image.width, image.height);	
-		
+			var dim = that.convert_dimensions(x, y, image.width, image.height);	
+			
 			//set up x, y to hold the global coordsinates
-			var coords = that.convert_coords(x, y);
-				x = coords.x; y = coords.y;
+			var coords = that.convert_coords(dim.x, dim.y);
+				dim.x = coords.x; dim.y = coords.y;
 
 			if(/fill/.test(mode)){			
 				that.scratch_ctx.drawImage(image, 0, 0, that.width, that.height);
@@ -155,7 +138,25 @@ function canvas_frame(parent_frame, width, height, percent){
 			}
 	}
 
-/*Fills the frame with the given color*/
+/** Draws the image data onto the frame 
+	* @param image_data an object of type image data that is to be drawn to the frame.
+	* @param x the x coordinate within the current frame of the image to be drawn.
+	* @param y the y coordinate within the current frame of the image to be drawn.
+**/
+	this.draw_image_data = function(image_data, x, y){
+		//check the types of the coordsinates, if not numbers, set to 0
+		if(typeof x_coords !== "number"){ x_coords = 0;}
+		if(typeof y_coords !== "number"){ y_coords = 0;}
+		if( !(image_data instanceof ImageData) ){return;}
+	
+		//change the dimensions if needed
+		var dim = that.convert_dimensions(x, y, image.width, image.height);	
+		
+	}
+
+/** Fills the frame with the given color 
+	
+**/
 	this.fill_color = function(color){
 			that.context.fillStyle = color;
 			that.context.fillRect(that.coords.x, that.coords.y, that.width, that.height);	
@@ -177,7 +178,7 @@ function canvas_frame(parent_frame, width, height, percent){
 		that.context.fillText(text, coords.x, coords.y); 
 	}
 
-  /*converts the given coords from inside this frame into global coords
+  /** Converts the given coords from inside this frame into global coords.
 	 * @return an object containing x and y coordsinate
 	*/
 	this.convert_coords = function(x_coord, y_coord){
@@ -188,5 +189,33 @@ function canvas_frame(parent_frame, width, height, percent){
 		if(that.parent_frame instanceof HTMLCanvasElement){	
 			return { x:x_coord, y:y_coord};
 		} else { return that.parent_frame.convert_coords(x_coord, y_coord);}
+	}
+
+	/** Returns a set of dimensions with which an image will fit within all frames 
+		* @return an object containing x, y coords the width, height and the clipping dimensions
+	**/
+	this.convert_dimensions = function(x, y, width, height, sx, sy, swidth, sheight){
+			//set up sx and sy
+			if(typeof sx !== "number" || typeof sy !== "number"){
+				sx = 0; sy = 0;}
+			if(x < 0 && sx < -x){ sx = -x; x = 0;}
+			if(y < 0 && sy < -y){ sy = -y; y = 0;}
+
+			//set up swidth and sheight
+			if(typeof swidth !== "number" || typeof sheight !== "number"){
+				swidth = 0; sheight = 0;}
+			if( (x + width) > that.width){ 
+				swidth = that.width - x;}
+			if( (y + height) > that.height){ 
+				sheight = that.height - y;}
+
+		//convert the coordinates into that of the parent's
+		x = x + that.coords.x;
+		y = y + that.coords.y;
+
+		if(!(that.parent_frame instanceof HTMLCanvasElement) ){
+			return that.parent_frame.convert_dimensions(x, y, width, height, sx, sy, swidth, sheight);}	
+				
+	return {x:x, y:y, width:width, height:height, sx:sx, sy:sy, swidth:swidth, sheight:sheight};
 	}
 }
